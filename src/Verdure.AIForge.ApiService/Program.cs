c3eb096000110fcbf345c0bb33b65e9bdd1529ef
Gil Zhang <gil.zhang.dev@outlook.com>
2025-05-08 00:57:32 +0800

    add botsharp 5.0

--- src/Verdure.AIForge.ApiService/Program.cs
+++ src/Verdure.AIForge.ApiService/Program.cs
@@ -1,9 +1,39 @@
+using BotSharp.Abstraction.Messaging.JsonConverters;
+using BotSharp.Core;
+using BotSharp.Logger;
+using BotSharp.OpenAPI;
+using BotSharp.Plugin.ChatHub;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+string[] allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>() ?? new[]
+    {
+        "http://0.0.0.0:5015",
+        "https://botsharp.scisharpstack.org",
+        "https://chat.scisharpstack.org"
+    };
+
+// Add BotSharp
+builder.Services.AddBotSharpCore(builder.Configuration, options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new RichContentJsonConverter());
+    options.JsonSerializerOptions.Converters.Add(new TemplateMessageJsonConverter());
+}).AddBotSharpOpenAPI(builder.Configuration, allowedOrigins, builder.Environment, true)
+  .AddBotSharpLogger(builder.Configuration);
+
+
 // Add service defaults & Aspire client integrations.
 builder.AddServiceDefaults();
+
+// Add SignalR for WebSocket
+builder.Services.AddSignalR()
+    // Enable Redis backplane for SignalR
+    /*.AddStackExchangeRedis("127.0.0.1", o =>
+    {
+        o.Configuration.ChannelPrefix = RedisChannel.Literal("botsharp");
+    })*/;
 
 // Add services to the container.
 builder.Services.AddProblemDetails();
@@ -12,6 +42,18 @@ builder.Services.AddProblemDetails();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+app.UseWebSockets();
+
+// Enable SignalR
+app.MapHub<SignalRHub>("/chatHub");
+//app.UseMiddleware<ChatHubMiddleware>();
+//app.UseMiddleware<ChatStreamMiddleware>();
+
+// Use BotSharp
+app.UseBotSharp()
+    .UseBotSharpOpenAPI(app.Environment)
+    .UseBotSharpUI();
 
 // Configure the HTTP request pipeline.
 app.UseExceptionHandler();
